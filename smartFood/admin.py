653cdc45e745c59bfbb9fd444e3cea25e182b900
Emanuel Real <malayadelossiglos7@gmail.com>
2024-03-12 11:47:28 -0600

    acualizacion v1.2

--- smartFood/admin.py
+++ smartFood/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categoria,Producto
+# mostar en el administrador las tablas de Producto yCategoria 
+admin.site.register(Categoria)
+
+#mostar campos de la tabla producto con sus registros 
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=('nombre' , 'precio' , 'categoria' , 'fecha_registro')
+    list_editable=('precio',)
+
